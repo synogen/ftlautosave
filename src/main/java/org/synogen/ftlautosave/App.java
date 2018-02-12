6c2eb9997aadc1fd5fa015a600a1f4b1326ec8fa
synogen <synogen@freenet.de>
2018-02-12 22:58:39 +0100

    initial commit / first functional version

---
+++ src/main/java/org/synogen/ftlautosave/App.java
@@ -0,0 +1,9 @@
+package org.synogen.ftlautosave;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        new Watch().start();
+    }
+}
---
+++ src/main/java/org/synogen/ftlautosave/Config.java
@@ -0,0 +1,9 @@
+package org.synogen.ftlautosave;
+
+public class Config {
+
+	public static final Integer WATCH_INTERVAL = 1000;
+	public static final String PROFILE_FILE = "ae_prof.sav";
+	public static final String SAVE_FILE = "continue.sav";
+	
+}
